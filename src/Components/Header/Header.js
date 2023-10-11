e3f9bc0fc2715522a02f7708b8c3252cff60a5b6
Evan Swanson <evan.sundelius@gmail.com>
2023-10-11 14:57:00 -0500

    Add buttons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,19 @@
 import './Header.css'
 import React, { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import githubImage from '../../resources/github_image_1.png'
 
 const Header = () => {
   return (
     <div className="Header">
-        <h1 className='title'>Tempestas<span className="title-sub">Mea</span></h1>
-        <a target='_blank' rel='noopener noreferrer' href="https://github.com/EvanSSwanson/myweather">Github</a>
+        <div className='header-left'>
+            <h1 className='title'>Tempestas<span className="title-sub">Mea</span></h1>
+            <button className='fahrenheit-button'>°F</button>
+            <button className='celcius-button'>°C</button>
+        </div>
+        <a target='_blank' rel='noopener noreferrer' href="https://github.com/EvanSSwanson/myweather">
+            <img src={githubImage} className= 'github-image'/>
+        </a>
     </div>
   )
 }
