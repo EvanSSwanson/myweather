82f8d14e009e0103fd1a6e06dab8bf71f23e1d70
Evan Swanson <evan.sundelius@gmail.com>
2023-10-26 10:56:13 -0500

    Finally find a working solution

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -7,51 +7,37 @@ import cities from '../../resources/cities'
 
 const Body = () => {
 
-    const [counter, setCounter] = useState(0)
-    const [info, setInfo] = useState([])
-    const [voila, setVoila] = useState([])
-    // const [info, setInfo] = useState([
-    //     {'Roscoe': {'weather': undefined, 'forecast': undefined}},
-    //     {'Chicago': {'weather': undefined, 'forecast': undefined}},
-    //     {'Rock%20Island': {'weather': undefined, 'forecast': undefined}},
-    //     {'Escanaba': {'weather': undefined, 'forecast': undefined}},
-    //     {'Montreal': {'weather': undefined, 'forecast': undefined}},
-    //     {'Longmont': {'weather': undefined, 'forecast': undefined}},
-    //     {'Liverpool': {'weather': undefined, 'forecast': undefined}},
-    //     {'Buenos%20Aires': {'weather': undefined, 'forecast': undefined}},
-    //     {'Tokyo': {'weather': undefined, 'forecast': undefined}},
-    //     {'Melbourne,AU': {'weather': undefined, 'forecast': undefined}}
-    // ])
+    const [info, setInfo] = useState([
+        {'weather': example[0], 'forecast': undefined},
+        {'weather': example[0], 'forecast': undefined},
+        {'weather': example[0], 'forecast': undefined},
+        {'weather': example[0], 'forecast': undefined},
+        {'weather': example[0], 'forecast': undefined},
+        {'weather': example[0], 'forecast': undefined},
+        {'weather': example[0], 'forecast': undefined},
+        {'weather': example[0], 'forecast': undefined},
+        {'weather': example[0], 'forecast': undefined},
+        {'weather': example[0], 'forecast': undefined},
+    ])
 
     const getData = (city, keyword, i) => {
         fetch(`https://api.openweathermap.org/data/2.5/${keyword}?q=${city}&appid=574c152960d1d19cafbfb4462369cf51&units=imperial`)
             .then(response => {
                 return response.json()
             })
-            // .then(data => setInfo(info => [...info, data]))
+            // .then(data => setInfo(info => [...info, data])
             .then(data => {
-                data.checker = i
-                setInfo(info => [...info, data])
+                // data.checker = i
+                setInfo([...info, info[i].weather = data])
             })
             .catch(error => {
                 console.log(error)
       })
       }
 
-      const cleanData = (input) => {
-        let grail = []
-        for (let i = 0; i < cities.length; i++) {
-            const piece = input.find(datum => datum.checker === i)
-            grail.push(piece)
-        }
-        return grail
-      }
-
       useEffect(() => {
-        for (let i = 0; i < cities.length + 1; i++) {
-            if (i < cities.length) {
+        for (let i = 0; i < cities.length; i++) {
                 getData(cities[i], 'weather', i)
-            } 
             // else {
             //     setTimeout(() => {
             //         setInfo(info.sort((a, b) => a.checker - b.checker))
@@ -71,13 +57,13 @@ const Body = () => {
   return (
     
     <div className='Body'>
-        {info.map((datum) => {
+        {info.filter(obj => obj.weather.base === 'stations').map((datum) => {
             return (
                 <Card 
-                city={datum.name}
-                region={datum.sys.country}
-                temperature={datum.main.temp}
-                pressure={datum.main.pressure}
+                city={datum.weather.name}
+                region={datum.weather.sys.country}
+                temperature={datum.weather.main.temp}
+                pressure={datum.weather.main.pressure}
                 />
             );
         })}
--- src/resources/startingData.js
+++ src/resources/startingData.js
@@ -1,46 +1,46 @@
 const example = [
     {
         coord: {
-        lon: -0.1257,
-        lat: 51.5085
+        lon: '',
+        lat: ''
         },
         weather: [
         {
-        id: 804,
-        main: "Clouds",
-        description: "overcast clouds",
-        icon: "04n"
+        id: '',
+        main: '',
+        description: '',
+        icon: ''
         }
         ],
         base: "stations",
         main: {
-        temp: 53.19,
-        feels_like: 51.64,
-        temp_min: 51.04,
-        temp_max: 55,
-        pressure: 1010,
-        humidity: 73
+        temp: '',
+        feels_like: '',
+        temp_min: '',
+        temp_max: '',
+        pressure: '',
+        humidity: ''
         },
-        visibility: 10000,
+        visibility: '',
         wind: {
-        speed: 18.41,
-        deg: 100
+        speed: '',
+        deg: ''
         },
         clouds: {
-        all: 100
+        all: ''
         },
-        dt: 1697577728,
+        dt: '',
         sys: {
-        type: 2,
-        id: 2006068,
-        country: "GB",
-        sunrise: 1697524016,
-        sunset: 1697562276
+        type: '',
+        id: '',
+        country: '',
+        sunrise: '',
+        sunset: ''
         },
-        timezone: 3600,
-        id: 2643743,
-        name: "London",
-        cod: 200
+        timezone: '',
+        id: '',
+        name: '',
+        cod: ''
     },
     {
         coord: {
