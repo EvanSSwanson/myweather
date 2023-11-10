b96105ea18ab5f694f36a10491e5a5c1a8aafb33
Evan Swanson <evan.sundelius@gmail.com>
2023-11-10 11:48:57 -0600

    continue stying optimization

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,6 +1,26 @@
 import './Card.css';
+import React from 'react'
+import { useCollapse } from 'react-collapsed';
 
 const Card = ({city, region, temperature, feelslike, description, pressure, time, dewpoint, humidity, direction, windspeed, windgust, icon}) => {
+
+  function Collapsible() {
+    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+return (
+    <div className="collapsible">
+        <div className='collapse-header' {...getToggleProps()}>
+            {isExpanded ? 'Collapse' : 'Expand'}
+        </div>
+        <div {...getCollapseProps()}>
+            <div className="collapse-content">
+                Now you can see the hidden content. <br/><br/>
+                Click again to hide...
+            </div>
+        </div>
+    </div>
+    );
+}
+
   return (
     <div className='Card' id={city}> 
         <div className='panel-top'>
@@ -11,11 +31,11 @@ const Card = ({city, region, temperature, feelslike, description, pressure, time
         <div className='main-panel-container'>
           <div className='panel-left'>
             <img className='icon' src={icon} />
-            <h2>{description}</h2>
+            <h2 className='description'>{description}</h2>
           </div>
           <div className='panel-center'>
             <h1 className='temperature'>{temperature}</h1>
-            <h2>feels like {feelslike}</h2>
+            <h2 className='feelslike'>feels like {feelslike}</h2>
           </div>
           <div className='panel-right'>
             <p className='dew-point'>dew point: {dewpoint}</p>
@@ -26,7 +46,8 @@ const Card = ({city, region, temperature, feelslike, description, pressure, time
           </div>
         </div>
         <div className='panel-bottom'>
-          <button className='button' id='forecast-button'>forecast</button>
+          {/* <button className='button' id='forecast-button'>forecast</button> */}
+          <Collapsible/>
         </div>
     </div>
   )
